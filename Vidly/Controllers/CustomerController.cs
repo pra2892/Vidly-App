4aa55dc44d6f7d0919e57dc3251cf7fc2c0c1cce
Dell <Dell@DESKTOP-I45THEF>
2025-07-31 14:20:47 +0530

    Add project files.

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [Route("customers")]
+        public ActionResult Index()
+        {
+            List<Customer> customer = new List<Customer>()
+            {
+                new Customer() { Id = 1, Name = "Prashant"},
+                new Customer() { Id = 2, Name = "Pallavi"}
+            };
+
+            return View(customer);
+        }
+
+        [Route("customers/details/{Id}")]
+        public ActionResult Details(int Id)
+        {
+            List<Customer> customer = new List<Customer>()
+            {
+                new Customer() { Id = 1, Name = "Prashant"},
+                new Customer() { Id = 2, Name = "Pallavi"}
+            };
+
+            List<Customer> searchCustomer = customer.Where(item => item.Id == Id).ToList();
+
+            return View(searchCustomer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [Route("movies")]
+        public ActionResult Random()
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie() { Id = 1, Name = "Pushpa"},
+                new Movie() { Id = 2, Name = "KGF"}
+            };
+
+            return View(movies);
+        }
+
+        [Route("movies/details/{Id}")]
+        public ActionResult Details(int Id)
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie() { Id = 1, Name = "Pushpa"},
+                new Movie() { Id = 2, Name = "KGF"}
+            };
+
+            List<Movie> movie = movies.Where(item => item.Id == Id).ToList();
+
+            return View(movie);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year +"/"+ month);
+        }
+
+    }
+}
